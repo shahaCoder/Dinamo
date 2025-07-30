1f3df772e594ed7f243f1882538391c41b4a8c03
Shoxrux Code <shoxruxpl04@gmail.com>
2025-07-29 20:18:42 -0400

    types were fixed

--- dinamo/app/matches/[id]/page.tsx
+++ dinamo/app/matches/[id]/page.tsx
@@ -1,25 +1,42 @@
 import Image from "next/image";
 import React from "react";
 
-const Page = ({ params }: { params: { id: string } }) => {
+// const Page = ({ params }: { params: { id: string } }) => {
+//    return (
+//       <div className="bg-[#0f2145]">
+//          <div className="custom-container py-20">
+//             <div className="">
+//                <p className="text-3xl max-md:text-2xl font-bold font-russo italic uppercase">
+//                   FC Dinamo
+//                </p>
+//                <Image
+//                   className=""
+//                   src="/logo.png"
+//                   width={60}
+//                   height={60}
+//                   alt="img"
+//                />
+//             </div>
+//          </div>
+//       </div>
+//    );
+// };
+const Page = (props: any) => {
+   const id = props?.params?.id;
+
    return (
       <div className="bg-[#0f2145]">
          <div className="custom-container py-20">
-            <div className="">
+            <div>
                <p className="text-3xl max-md:text-2xl font-bold font-russo italic uppercase">
-                  FC Dinamo
+                  FC Dinamo — ID: {id}
                </p>
-               <Image
-                  className=""
-                  src="/logo.png"
-                  width={60}
-                  height={60}
-                  alt="img"
-               />
+               <Image src="/logo.png" width={60} height={60} alt="img" />
             </div>
          </div>
       </div>
    );
 };
+
 
 export default Page;
