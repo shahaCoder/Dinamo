215302681d28f90b14cca1faf08f2b8c5c6a149d
Shoxrux Code <shoxruxpl04@gmail.com>
2025-07-31 16:12:50 -0400

    fixed few errors

--- control_panel/src/app/layout.tsx
+++ control_panel/src/app/layout.tsx
@@ -16,10 +16,10 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         <div className="flex justify-between relative">
-          <aside className="w-[350px] bg-[rgba(3,163,239,1)] h-full self-stretch">
+          <aside className="w-[350px] bg-[rgba(3,163,239,1)] h-screen self-stretch">
             <Navbar />
           </aside>
-          <main className="w-full p-4 bg-white h-screen">
+          <main className="w-full p-4 bg-white h-screen overflow-y-auto">
             {children}
           </main>
         </div>
